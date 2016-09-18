745d1c2ff28b86c4e93ac07f8355c4ebca48af0a
ashahhar <shahhara1989@gmail.com>
2016-09-17 20:49:53 -0600

    unique number in pairs

---
+++ src/uniqNum.java
@@ -0,0 +1,54 @@
+package com.user.xml;
+
+//Consider an array of  integers, , where all but one of the integers occur in pairs. In other words, 
+//every element in  occurs exactly twice except for one unique element.
+//Given , find and print the unique element.
+//Input Format:
+//The first line contains a single integer, , denoting the number of integers in the array. 
+//The second line contains  space-separated integers describing the respective values in .
+//Constraints
+//It is guaranteed that  is an odd number.
+//, where .
+//Output Format:	
+//	Print the unique number that occurs only once in  on a new line.
+//Sample Input:
+//	3
+//	1 1 2
+//	
+//Sample Output:
+//	2
+//Link to question:  https://www.hackerrank.com/challenges/lonely-integer
+	
+	import java.io.*;
+	import java.util.*;
+	import java.text.*;
+	import java.math.*;
+	import java.util.regex.*;
+
+	public class uniqNum {
+	     private static int lonelyInteger(int[] a) {
+	         //HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+	        List<Integer> myList = new ArrayList<Integer>();
+	         for(int i=0; i<a.length; i++){
+	             if(myList.contains(a[i])){
+	                 myList.remove(Integer.valueOf(a[i]));
+	             }else{
+	                 myList.add(a[i]);
+	             }
+	         }
+	         return myList.get(0);
+	        
+	    }
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt();
+	        int[] a = new int[n];
+	        for (int i = 0; i < n; i++) {
+	            a[i] = in.nextInt();
+	        }
+	        System.out.println(lonelyInteger(a));
+	    }
+	}
+
+
+
